5f7ce84a421b3c9cd3d8e4573c23d42044bb28b9
KunalKatiyar <katiyarkunal2011@gmail.com>
2022-09-07 21:36:26 +0530

    Created 2021 package

---
+++ year-2021/target/debug/deps/year_2021.d
@@ -0,0 +1,5 @@
+D:\Dektop\GitProjects\advent-of-code-in-rust\year-2021\target\debug\deps\year_2021.exe: src\main.rs
+
+D:\Dektop\GitProjects\advent-of-code-in-rust\year-2021\target\debug\deps\year_2021.d: src\main.rs
+
+src\main.rs:
---
+++ year-2021/target/debug/year-2021.d
@@ -0,0 +1,1 @@
+D:\Dektop\GitProjects\advent-of-code-in-rust\year-2021\target\debug\year-2021.exe: D:\Dektop\GitProjects\advent-of-code-in-rust\year-2021\src\main.rs
